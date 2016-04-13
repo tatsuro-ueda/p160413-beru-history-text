6a4137705fed34a8316e61c84c91e1833cf83e14
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2016-04-13 12:39:31 +0900

    🚧画像の指定をjsonの中でできるようにした。複数画像や地図埋め込みに未対応。

--- 160411-beru-buttonized.ts
+++ 160411-beru-buttonized.ts
@@ -21,10 +21,15 @@ var jQuery: any
     };
 
     var dic = {
+        'ここを押してみて下さい':
+        {
+            type: 'この記事の読み方',
+            content: '説明が画面の外に出るときは吹き出しを引っ張って下さい。なお、<button>重要語句</button>というように他の語句へのボタンがあります。活用して下さい。'
+        },
         '重要語句':
         {
             type: 'この記事の読み方',
-            content: '世界史を学習する上で外すことはできません。<strong>必ず</strong>理解して下さい。なお、<button>深掘り語句</button>というように他の語句へのボタンがあります。活用して下さい。'
+            content: '世界史を学習する上で外すことはできません。<strong>必ず</strong>理解して下さい。'
         },
         '深掘り語句':
         {
@@ -39,21 +44,31 @@ var jQuery: any
         'オルレアン公':
         {
             type: '人物',
-            content: imgSmall('20160328/20160328114737.jpg', 'p')
-            + 'フランスの王族で国王のいとこにあたり、王国有数の富豪であった。公爵は、その財力をもって王位を狙う野心家で、革命前に最初に国王に逆らい<button>自由主義</button>貴族の代表となった。バスティーユ襲撃事件を誘発し、フランス革命が勃発すると歓迎して「平等公フィリップ」を自称した。なお、私生活は放蕩かつ無節操だった。'
+            img: {
+                filename: '20160328/20160328114737.jpg',
+                orientation: 'p'
+            },
+            content: 'フランスの王族で国王のいとこにあたり、王国有数の富豪であった。公爵は、その財力をもって王位を狙う野心家で、革命前に最初に国王に逆らい<button>自由主義</button>貴族の代表となった。バスティーユ襲撃事件を誘発し、フランス革命が勃発すると歓迎して「平等公フィリップ」を自称した。なお、私生活は放蕩かつ無節操だった。'
         },
         'ハプスブルク家':
         {
             type: '歴史',
-            content: imgSmall('20160328/20080401153911.jpg', 'l',
-                '1547年時点でのハプスブルク家の領土')
-            + 'ヨーロッパ随一の名門王家。政略結婚により大貴族に成長した。20世紀初頭まで中部ヨーロッパで強大な勢力を誇り、様々な国の国王・皇帝の家系となった。また、神聖ローマ帝国の皇帝位を中世以来保持した。最後はビスマルクが排除した。'
+            img: {
+                filename: '20160328/20080401153911.jpg',
+                orientation: 'l',
+                caption: '1547年時点でのハプスブルク家の領土'
+            },
+            content: 'ヨーロッパ随一の名門王家。政略結婚により大貴族に成長した。20世紀初頭まで中部ヨーロッパで強大な勢力を誇り、様々な国の国王・皇帝の家系となった。また、神聖ローマ帝国の皇帝位を中世以来保持した。最後はビスマルクが排除した。'
         },
         '第三身分':
         {
             type: '歴史',
-            content: imgSmall('20160328/20130210221541.jpg', 'p', '<button>アンシャン・レジーム</button>を風刺した画')
-            + '3つの身分のうちもっとも低い平民を意味する。'
+            img: {
+                filename: '20160328/20130210221541.jpg',
+                orientation: 'p',
+                caption: '<button>アンシャン・レジーム</button>を風刺した画'
+            },
+            content: '3つの身分のうちもっとも低い平民を意味する。'
             + '<ol>'
             + '<li>第一身分：聖職者</li>'
             + '<li>第二身分：貴族</li>'
@@ -63,8 +78,12 @@ var jQuery: any
         'アンシャン・レジーム':
         {
             type: '歴史',
-            content: imgSmall('20160328/20130210221541.jpg', 'p', 'アンシャン・レジームを風刺した画')
-            + '2％の聖職者＋貴族（免税権も持っていた）を、98％の平民が背負う世情'
+            img: {
+                filename: '20160328/20130210221541.jpg',
+                orientation: 'p',
+                caption: '聖職者と貴族を、平民が背負って歩いている'
+            },
+            content: '直訳すると「旧（アンシャン）体制（レジーム）」。2％の聖職者＋貴族（免税権も持っていた）を、98％の平民が背負っていた。'
         },
         '自由主義':
         {
@@ -80,19 +99,22 @@ var jQuery: any
         '絶対王政':
         {
             type:'歴史',
-            content: imgSmall('20160328/20120816105356.jpg', 'l',
-                '自由主義・国民主権の前は<button>絶対王政</button>だった（エリザベス1世）')
-            + '諸侯や貴族・教会の権力が地方に乱立していた状態から、強大な権力を持って中央集権化を図り、中央官僚と常備軍（<button>近衛隊</button>）によって国家統一を成し遂げた体制のこと'
+            img:{
+                filename: '20160328/20120816105356.jpg',
+                orientation: 'l',
+                caption: '<button>自由主義</button>・国民主権の前は絶対王政だった（エリザベス1世）'
+            },
+            content: '諸侯や貴族・教会の権力が地方に乱立していた状態から、強大な権力を持って中央集権化を図り、中央官僚と常備軍（<button>近衛隊</button>）によって国家統一を成し遂げた体制のこと'
         },
         'ブルボン家':
         {
             type: '歴史',
-            content: imgSmall('20160328/20160401134146.jpg', 'p',
-                '「太陽王」ルイ14世の肖像画')
-            + '近世フランス王家。</p>'
-            + '<p><button>メディチ家</button>の家系で、その財力を受け継いだ。'
-            + '<p>「太陽王」ルイ14世は、<button>絶対王政</button>を敷いた。</p>'
-            + '<p>さらに<button>ハプスブルク家</button>と政略結婚し、領土を拡大して最盛期を迎え、フランス文化を発展させた。</p>'
+            img: {
+                filename: '20160328/20160401134146.jpg',
+                orientation: 'p',
+                caption: '「太陽王」ルイ14世の肖像画'
+            },
+            content: '近世フランス王家。<button>メディチ家</button>の家系で、その財力を受け継いだ。「太陽王」ルイ14世は、<button>絶対王政</button>を敷いた。さらに<button>ハプスブルク家</button>と政略結婚し、領土を拡大して最盛期を迎え、フランス文化を発展させた。'
         },
         'メディチ家':
         {
@@ -102,10 +124,13 @@ var jQuery: any
         'ルソー':
         {
             type: '人物',
-            content: imgSmall('20160328/20160328125712.jpg', 'p')
-            + 'ジャン・ジャック・ルソー。'
+            img: {
+                filename: '20160328/20160328125712.jpg',
+                orientation: 'p'
+            },
+            content:'ジャン・ジャック・ルソー。'
             + '18世紀フランスで活躍した哲学者、政治哲学者、作曲家。'
-            + '「<strong><button>社会契約論（ルソー）</button></strong>」を説いた'
+            + '「<button>社会契約論（ルソー）</button>」を説いた'
             + '（ホッブも同名の本を書いているので注意）。'
             + 'なお、「ベルサイユのばら」の時点ではすでに亡くなっている。'
         },
@@ -123,27 +148,39 @@ var jQuery: any
         'ロベスピエール':
         {
             type: '人物',
-            content: imgSmall('20160328/20160330180206.jpg', 'p')
-            + '地方で弁護士をしていたが、1789年三部会の議員に選出されてから革命に身を投じる。'
+            img: {
+                filename: '20160328/20160330180206.jpg',
+                orientation: 'p'
+            },
+            content: '地方で弁護士をしていたが、1789年三部会の議員に選出されてから革命に身を投じる。'
         },
         'オペラ座':
         {
             type: '場所',
+            img: {
+                filename: '20160328/20120926141919.jpg',
+                orientation: 'l'
+            },
             content: '<p><iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sja!2sjp!4v1459148169706!6m8!1m7!1sVfs3uZuxLHWwCPjmt-pXsw!2m2!1d48.87111407616459!2d2.33216326243794!3f343.62178183560997!4f16.939412639927582!5f0.7820865974627469" frameborder="0" style="border:0" allowfullscreen></iframe></p>'
             + '<p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=zxuorCdgTka8.kQkyzRxYdq3Y"></iframe></p>'
-            + imgSmall('20160328/20120926141919.jpg', 'l')
             + 'パリにある歌劇場。17世紀から多くの作品を公演した。しかし後継の親族らの経営は低迷し、赤字は積もった。革命期には、迎合的な作品も上演された。経営者が次々と代わり、ナポレオン政府の経済的措置でようやく救われた。実はガルニエ宮が完成したのは19世紀末。革命の1世紀後のことです。現在のオペラ座はさらに新しく、20世紀末に完成したオペラ・バスティーユです。観光に行くときはご注意を。'
         },
         '仮面舞踏会':
         {
             type:'時代',
-            content: imgSmall('20160328/20160328125651.jpg', 'l')
-            + 'ルネサンス期のイタリアの、参加者が仮装して出席する公的な祭典が発祥。ゲームとして開催されることもあった。仮面をした客たちは正体が誰か分からないような服装をし、互いの正体を当てあうゲームを行った。'
-            + '「風紀が乱れる」と、禁止されることもあったそうです。どんなことをしていたんだろう・・・少年の僕にはわからないなあ。'
+            img: {
+                filename: '20160328/20160328125651.jpg',
+                orientation: 'l'
+            },
+            content: 'ルネサンス期のイタリアの、参加者が仮装して出席する公的な祭典が発祥。ゲームとして開催されることもあった。仮面をした客たちは正体が誰か分からないような服装をし、互いの正体を当てあうゲームを行った。「風紀が乱れる」と、禁止されることもあったそうです。どんなことをしていたんだろう・・・少年の僕にはわからないなあ。'
         },
         'パレ・ロワイヤル':
         {
             type:'場所',
+            img: {
+                filename: '20160328/20160328115556.jpg',
+                portrait: 'l'
+            },
             content:
             '<p><iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sja!2sjp!4v1459487609649!6m8!1m7!1sa37NF5mxyuTWHIA3VTUgow!2m2!1d48.86278015564088!2d2.337007608338476!3f20.846674658759838!4f11.022677808318875!5f0.7820865974627469" frameborder="0" style="border:0" allowfullscreen></iframe></p>'
             + '<p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=zxuorCdgTka8.kZM0bOW34Q-w"></iframe></p>'
@@ -156,31 +193,41 @@ var jQuery: any
                 '内部（クリックするとストリートビューになります）')
             + '</a><br clear="left" />'
             + '<p>'
-            + imgSmall('20160328/20160328115556.jpg', 'l')
             + 'ルイ14世がルーヴル宮殿から移り住んだことで、パレ・ロワイヤル（王宮）と呼ばれるようになった。庶民は庭園で散歩を楽しむことができた。'
             + '</p>'
         },
         '近衛隊':
         {
             type:'時代',
-            content:imgSmall('20160330/20160330174323.jpg', 'l', 'イギリスの近衛兵')
-            + '<br clear="left" />'
+            img: {
+                filename: '20160330/20160330174323.jpg',
+                orientation: 'l',
+                caption: 'イギリスの近衛兵'
+            },
+            content:
+            '<br clear="left" />'
             + imgSmall('20070616/20070616111017.jpg', 'l')
             + '国王の身辺警護をする軍隊の一部のエリート部隊。特に体制寄りで保守的な隊員で固められているでしょうから、その隊長であるオスカルが来たことが意外だったのでしょう。'
         },
         '連隊':
         {
             type:'時代',
-            content:imgSmall('20160330/20160330174858.jpg', 'p',
-                '連隊はそれぞれ独自の旗を持って戦った')
-            + '二千人程度で編成され、地域や貴族指揮官の名称が付けられた。採用や給与、兵站等あらゆる手続きが連隊内で完結していた。連隊の指導者である大佐の上は国王だけだった。このためマリー・アントワネットは王権を使って各地の連隊を革命直前のパリ警戒に召集することができた。'
+            img: {
+                filename: '20160330/20160330174858.jpg',
+                orientation: 'p',
+                caption: '連隊はそれぞれ独自の旗を持って戦った'
+            },
+            content: '二千人程度で編成され、地域や貴族指揮官の名称が付けられた。採用や給与、兵站等あらゆる手続きが連隊内で完結していた。連隊の指導者である大佐の上は国王だけだった。このためマリー・アントワネットは王権を使って各地の連隊を革命直前のパリ警戒に召集することができた。'
         },
         'ラテン語':
         {
             type:'時代',
-            content:imgSmall('20160330/20160330175018.jpg', 'l',
-                '「誤るのが人間である」（古代ローマ）')
-            + '近代においても広く欧州知識人の公用語として用いられた。古い著作の中にはラテン語で記されたものもあり、たとえば自然科学ではニュートンのプリンキピアがある。欧州諸国では第二次大戦まではラテン語が必修だったが、現在では日本での「古典」「古文」ないし「漢文」に相当する科目として存在する程度である。理系の私は「漢文」がとても苦手でした。しかし中国文化圏では漢字で筆談ができるのと同じように、西欧文化圏ではラテン語が（お互いに教育水準が高ければ）通じるんでしょうね。'
+            img: {
+                filename: '20160330/20160330175018.jpg',
+                orientation: 'l',
+                caption: '「誤るのが人間である」（古代ローマ）'
+            },
+            content:'近代においても広く欧州知識人の公用語として用いられた。古い著作の中にはラテン語で記されたものもあり、たとえば自然科学ではニュートンのプリンキピアがある。欧州諸国では第二次大戦まではラテン語が必修だったが、現在では日本での「古典」「古文」ないし「漢文」に相当する科目として存在する程度である。理系の私は「漢文」がとても苦手でした。しかし中国文化圏では漢字で筆談ができるのと同じように、西欧文化圏ではラテン語が（お互いに教育水準が高ければ）通じるんでしょうね。'
         },
         '早馬車':
         {
@@ -190,40 +237,86 @@ var jQuery: any
         '駅馬車':
         {
             type:'時代',
-            content:imgSmall('20160330/20160330175142.jpg', 'l',
-                'スイスの駅馬車')
-                + '駅馬車は通常、4頭立ての馬に牽引された、旅客や貨物を輸送する屋根つき馬車である。駅馬車の車体は革ひもで支えられており、衝撃を吸収した。街道には一定の間隔で駅が設置された。次の駅では休養の十分な馬の組が待機しており、馬をつなぎかえるだけの短い停車時間で旅行を続けられるようになっていた。'
+            img: {
+                filename: '20160330/20160330175142.jpg',
+                orientation: 'l',
+                caption: 'スイスの駅馬車'
+            },
+            content:'駅馬車は通常、4頭立ての馬に牽引された、旅客や貨物を輸送する屋根つき馬車である。駅馬車の車体は革ひもで支えられており、衝撃を吸収した。街道には一定の間隔で駅が設置された。次の駅では休養の十分な馬の組が待機しており、馬をつなぎかえるだけの短い停車時間で旅行を続けられるようになっていた。'
         },
         '王家の馬車だった':
         {
             type:'台詞',
-            content:imgSmall('20160330/20160330175357.png', 'l',
-                'フランス王家などが紋章として用いたアヤメの意匠')
-            + 'オスカルは、すれ違った馬車にアヤメの紋章が意匠されていることに気づき、フランス王家の馬車だと判断したのでしょう。'
+            img: {
+                filename: '20160330/20160330175357.png',
+                orientation: 'l',
+                caption: 'フランス王家などが紋章として用いたアヤメの意匠'
+            },
+            content:'オスカルは、すれ違った馬車にアヤメの紋章が意匠されていることに気づき、フランス王家の馬車だと判断したのでしょう。'
         },
         'ベルナール':
         {
             type:'人物',
-            content:imgSmall('20160330/20160330175853.jpg', 'p',
-                'カミーユ・デムーランの肖像')
-            + 'ベルナール・シャトレ。架空の人物だが、弁護士でジャーナリストのカミーユ・デムーランという人がモデル。ルイ・ル・グラン学校では<button>ロベスピエール</button>と机を並べて学んだ。'
+            img: {
+                filename: '20160330/20160330175853.jpg',
+                orientation: 'p',
+                caption: 'カミーユ・デムーランの肖像'
+            },
+            content:'ベルナール・シャトレ。架空の人物だが、弁護士でジャーナリストのカミーユ・デムーランという人がモデル。ルイ・ル・グラン学校では<button>ロベスピエール</button>と机を並べて学んだ。'
         }
 
     };
 
     var keyword: string;
 
-    $(function() {
+    function imgSmall2(obj) {
+        if (obj == null) {
+            return ''
+        } else {
+            var oHtml: string;
+            if (obj.orientation === 'p'){
+                oHtml = '" height="200px"'
+            } else {
+                oHtml = '" width="200px"'
+            }
+            var capHtml: string;
+            if (obj.caption == null){
+                capHtml = '';
+            } else {
+                capHtml = '<p><em>' + obj.caption + '</em></p>';
+            };
+            var imgHtml = '<p><img style="margin-right: 10px; margin-bottom: 10px" src="http://cdn-ak.f.st-hatena.com/images/fotolife/w/weed_7777/'
+                + obj.filename + oHtml + ' align="left"></p>' + capHtml;
+            return imgHtml;
+        }
+    }
 
+    function contentHtml(keyword) {
+        var imgHtml: string = imgSmall2(keyword.img);
+        var cntHtml = imgHtml + keyword.content;
+        var btnedHtml: string = cntHtml.replace(
+            /<button>/g,
+            '<button class="button button-rounded button-primary">');
+        return btnedHtml;
+    }
+
+    $(function() {
         for (keyword in dic) {
             $('button:contains('+ keyword + ')')
-            .attr({
-                'data-title': '【' + dic[keyword].type + '】' + keyword, 
-                'data-content': dic[keyword].content,
-                'data-width': 400,
-                'data-multi': true
-            })
-            .webuiPopover()
+                .attr({
+                    'data-title': '【' + dic[keyword].type + '】' + keyword,
+                    'data-content': contentHtml(dic[keyword]),
+                    // imgSmall2(dic[keyword].img) + dic[keyword].content.replace(
+                    //     /<button>/g,
+                    //     '<button class="button button-rounded button-primary">'),
+                    // 'data-content': dic[keyword].content.replace(
+                    //     /<button>/g,
+                    //     '<button class="button button-rounded button-primary">'),
+                    'data-width': 400,
+                    'data-multi': true,
+                    'class': 'button button-rounded button-primary'
+                })
+                .webuiPopover()
         }
         
         // function buttonize() {
@@ -252,9 +345,14 @@ var jQuery: any
         for (keyword in dic) {
             if ($(this).text() === keyword) {
                 $(this)
+                    .attr({
+                        'class': 'button button-rounded button-primary'
+                    })
                     .webuiPopover({
                         title: '【' + dic[keyword].type + '】' + keyword,
-                        content: dic[keyword].content,
+                        content: contentHtml(dic[keyword]),
+                        // imgSmall2(dic[keyword].img) + dic[keyword].content.replace(
+                        //     /<button>/g),
                         trigger: 'manual',
                         width: 400,
                         multi: true
